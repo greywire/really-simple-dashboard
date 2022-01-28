2c1079af3bb8e94b2a0af4bb853b4a4307b0a6e8
Aric Caley <acaley@peoplecatcher.net>
2022-01-28 09:17:02 -0800

    Added half size scaler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faRedo } from '@fortawesome/free-solid-svg-icons';
+import { faCocktail, faCompress, faRedo } from '@fortawesome/free-solid-svg-icons';
 import { finalize } from 'rxjs/operators';
 
 import { QuoteService } from './quote.service';
@@ -8,6 +8,7 @@ class widget {
   title: string = '';
   url: string = '';
   width: number = 6;
+  scalehalfsize: boolean = false;
 }
 
 @Component({
@@ -25,11 +26,13 @@ export class HomeComponent implements OnInit {
         title: 'Acura OSS',
         url: 'http://10.1.20.54:4203/customer-appointment/customer-info?token=8a0cc9e77840086c903cc5c10c2b8522',
         width: 6,
+        scalehalfsize: false,
       },
       {
         title: 'Honda OSS',
         url: 'http://10.1.20.54:4203/customer-appointment/customer-info?token=8a7ddce709914581bafc23e9ed24ef2b',
         width: 6,
+        scalehalfsize: false,
       },
     ],
     [
@@ -37,16 +40,20 @@ export class HomeComponent implements OnInit {
         title: 'Lexus',
         url: 'http://10.1.20.54:4203/customer-appointment/customer-info?token=51f4ac4f6245c116cefe70133df57dbd',
         width: 6,
+        scalehalfsize: false,
       },
       {
         title: 'Toyota Smartpath',
         url: 'http://10.1.20.54:4203/customer-appointment/customer-info?token=3f7edcb0867c9f4443aa064dc8d1af36',
         width: 6,
+        scalehalfsize: false,
       },
     ],
   ];
 
   faRedo = faRedo;
+  faCompress = faCompress;
+  scalehalfsize = false;
 
   constructor(private quoteService: QuoteService) {}
 
@@ -68,5 +75,9 @@ export class HomeComponent implements OnInit {
     let elem: HTMLIFrameElement = <HTMLIFrameElement>document.getElementById(row + '_' + col);
 
     elem.src = this.widgets[row][col].url;
+  }
+
+  setscale(row: number, col: number) {
+    this.widgets[row][col].scalehalfsize = !this.widgets[row][col].scalehalfsize;
   }
 }
